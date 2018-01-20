0666545833017b8535ac78b81dc14f19e7ad07ac
Christoph Friedrich <christoph@friedrich-whv.de>
2018-01-20 23:08:00 +0100

    cleanup code
    - define functions in chronological order
    - remove old stuff (bboxes)

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var editableLayers;
 var drawControl;
-var bboxes;
 var sentinel;
 var polygon;
 var totalcount;
@@ -21,10 +20,29 @@ function getPolygonCoords(footprint) {
   .map((e)=>[e[1],e[0]]);
 }
 
-function showFootprintOnMap(event) {
-  var polygonlatlngs = JSON.parse(event.target.parentElement.dataset.polygoncoords);
-  polygon.setLatLngs(polygonlatlngs);
-  map.panInsideBounds(polygon.getBounds());
+function getSearchResults() {
+  $.get('http://gis-bigdata:11016/datasets', function(result) {
+    document.getElementById('searchresults').innerHTML = result
+    .sort((e1,e2) => e1.MTD.metadata[''].PRODUCT_START_TIME < e2.MTD.metadata[''].PRODUCT_START_TIME)
+    .map((e) =>
+      `<li
+        onclick="showFootprintOnMap(event)"
+        ondblclick="showDetails(event)"
+        data-footprint="${e.MTD.metadata[''].FOOTPRINT}"
+        data-polygoncoords="${JSON.stringify(getPolygonCoords(e.MTD.metadata[''].FOOTPRINT))}"
+        data-tmsurls="${(e.tmsUrls.R10m != undefined ? Object.values(e.tmsUrls.R10m).join(',').concat(Object.values(e.tmsUrls.R20m).join(',').concat(Object.values(e.tmsUrls.R60m).join(','))) : Object.values(e.tmsUrls).join(','))}"
+        data-scenename="${e.sceneName}"
+        data-datetime="${e.MTD.metadata[''].DATATAKE_1_DATATAKE_SENSING_START}"
+        data-cloudcoverage="${e.MTD.metadata[''].CLOUD_COVERAGE_ASSESSMENT}">
+        <strong>${e.sceneName.replace(/_/g, '_&#8203;')}</strong>
+        <br>
+        ${new Date(e.MTD.metadata[''].PRODUCT_START_TIME).toLocaleString()}
+      </li>`
+    ).join('\r\n');
+    totalcount = result.length;
+    document.getElementById('resultcount').innerHTML = totalcount;
+    filterResults();
+  });
 }
   
 function filterResults() {
@@ -47,6 +65,12 @@ function filterResults() {
       e.classList.remove('invisible');
   });
   document.getElementById('resultcount').innerHTML = totalcount - parseInt($('.invisible').length);
+}
+
+function showFootprintOnMap(event) {
+  var polygonlatlngs = JSON.parse(event.target.parentElement.dataset.polygoncoords);
+  polygon.setLatLngs(polygonlatlngs);
+  map.panInsideBounds(polygon.getBounds());
 }
 
 function showDetails(event) {
@@ -162,14 +186,6 @@ function initMap() {
     editableLayers.clearLayers();  // remove old rectangle when drawing a new one
   });
   
-  // SEARCH BBOXES
-  bboxes = L.layerGroup();
-  L.rectangle([[52,7],[53,8]]).addTo(bboxes);
-  L.rectangle([[54,8],[55,9]]).addTo(bboxes);
-  L.rectangle([[52.5,12.5],[53.5,13.5]]).addTo(bboxes);
-  L.rectangle([[48,10],[49,11]]).addTo(bboxes);
-  L.rectangle([[48.5,12],[49.5,13]]).addTo(bboxes);
-  
   // ORIGINAL VALUE
   map.on('click', function(e) {
     L.marker(e.latlng).bindPopup('Original value:<br>42.1337').addTo(map).openPopup();
@@ -180,8 +196,8 @@ function initMap() {
   
   // LOADING SYMBOL
   var loadingControl = L.Control.loading({
-      position: 'topright',
-      separate: true
+    position: 'topright',
+    separate: true
   });
   map.addControl(loadingControl);
 }
@@ -191,47 +207,46 @@ function initPanels() {
   sidebar.addPanel({
     id: 'home',
     tab: '<i class="fa fa-home fa-lg"></i>',
+    position: 'top',
     pane: `
-<h3>Amun Sphinx</h3>
-<p>Explore the wealth of Sentinel-2 data available and use their full potential!</p>
-<p>Click the maginfying glass on the left to start browsing the datasets.</p>
-    `,
-    position: 'top'
+      <h3>Amun Sphinx</h3>
+      <p>Explore the wealth of Sentinel-2 data available and use their full potential!</p>
+      <p>Click the maginfying glass on the left to start browsing the datasets.</p>`
   });
   
   // SEARCH panel
   sidebar.addPanel({
     id: 'search',
     tab: '<i class="fa fa-search fa-lg"></i>',
+    position: 'top',
     pane: `
-<h3>Search</h3>
-<form onsubmit="return false">
-  <input placeholder="Identifier" id="identifier" onkeyup="filterResults()">
-  <input placeholder="Start date" id="startdate" onchange="filterResults()" data-toggle="datepicker">
-  <input placeholder="End date" id="enddate" onchange="filterResults()" data-toggle="datepicker">
-  <input type="submit" value="Filter" onclick="filterResults()">
-</form>
-<h3>Results (<span id="resultcount"></span>)</h3>
-<ol id='searchresults'>
-  <li><strong>S2A_MSIL2A N0205_R108_T32UMC</strong><br>2017-09-27 10:30:21</li>
-  <li><strong>S2A_MSIL2A N0205_R109_T32UMC</strong><br>2017-09-27 10:35:18</li>
-  <li><strong>S2A_MSIL2A N0205_R110_T32UMC</strong><br>2017-09-27 10:40:27</li>
-  <li><strong>S2A_MSIL2A N0205_R111_T32UMC</strong><br>2017-09-27 10:45:22</li>
-  <li><strong>S2A_MSIL2A N0205_R112_T32UMC</strong><br>2017-09-27 10:50:13</li>
-</ol>
-<nav>
-  <button>&lt;</button>
-  <div>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-  </div>
-  <button>&gt;</button>
-</nav>
-    `,
-    position: 'top'
+      <h3>Search</h3>
+      <form onsubmit="return false">
+        <input placeholder="Identifier" id="identifier" onkeyup="filterResults()">
+        <input placeholder="Start date" id="startdate" onchange="filterResults()" data-toggle="datepicker">
+        <input placeholder="End date" id="enddate" onchange="filterResults()" data-toggle="datepicker">
+        <input type="submit" value="Filter" onclick="filterResults()">
+      </form>
+      <h3>Results (<span id="resultcount"></span>)</h3>
+      <ol id='searchresults'>
+        <li><strong>S2A_MSIL2A N0205_R108_T32UMC</strong><br>2017-09-27 10:30:21</li>
+        <li><strong>S2A_MSIL2A N0205_R109_T32UMC</strong><br>2017-09-27 10:35:18</li>
+        <li><strong>S2A_MSIL2A N0205_R110_T32UMC</strong><br>2017-09-27 10:40:27</li>
+        <li><strong>S2A_MSIL2A N0205_R111_T32UMC</strong><br>2017-09-27 10:45:22</li>
+        <li><strong>S2A_MSIL2A N0205_R112_T32UMC</strong><br>2017-09-27 10:50:13</li>
+      </ol>
+      <nav>
+        <button>&lt;</button>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+        <button>&gt;</button>
+      </nav>`
   });
   
+  // DATE PICKER for search panel
   $.fn.datepicker.languages['de-DE'] = {
     days: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
     daysShort: ['Son', 'Mon', 'Die', 'Mit', 'Don', 'Fre', 'Sam'],
@@ -245,88 +260,63 @@ function initPanels() {
   sidebar.addPanel({
     id: 'details',
     tab: '<i class="fa fa-picture-o fa-lg"></i>',
+    position: 'top',
     pane: `
-<h3>Details</h3>
-
-<div>
-  <strong>Identifier:</strong> <em id="details-identifier"></em>
-</div>
-
-<div>
-  <strong>Captured:</strong> <span id="details-datetime"></span><br>
-  <strong>Cloud Coverage:</strong> <span id="details-cloudcoverage"></span>&nbsp;%<br>
-  <!--<strong>UTM Zone:</strong><br>-->
-</div>
-
-<div>
-  <strong>Opacity:</strong>
-  <input type="range" min="0" max="100" step="1" value="100" id="details-opacity" onchange="changeOpacity(event)" />
-  <span>100&nbsp;%</span>
-</div>
-
-<div>
-  <strong>Color mode:</strong>
-  <input type="radio" name="colormode" id="grayscale" value="grayscale" checked/><label for="grayscale">Grayscale</label>
-  <input type="radio" name="colormode" id="rgb" value="rgb"/><label for="rgb">RGB</label>
-  <div>
-    <strong>Band to display:</strong> <select id="details-availablebands" onchange="changeTmsUrl()"></select>
-    <input placeholder="min" id="contrast-min" onchange="changeTmsUrl()">
-    <input placeholder="max" id="contrast-max" onchange="changeTmsUrl()">
-  </div>
-  <div>
-    <table>
-      <tr><td><strong>Red:  </strong></td><td><select id="details-availablebands-red"   onchange="changeTmsUrl()"></select></td><td><input placeholder="min" id="contrast-min-r" onchange="changeTmsUrl()"></td><td><input placeholder="max" id="contrast-max-r" onchange="changeTmsUrl()"></td></tr>
-      <tr><td><strong>Green:</strong></td><td><select id="details-availablebands-green" onchange="changeTmsUrl()"></select></td><td><input placeholder="min" id="contrast-min-g" onchange="changeTmsUrl()"></td><td><input placeholder="max" id="contrast-max-g" onchange="changeTmsUrl()"></td></tr>
-      <tr><td><strong>Blue: </strong></td><td><select id="details-availablebands-blue"  onchange="changeTmsUrl()"></select></td><td><input placeholder="min" id="contrast-min-b" onchange="changeTmsUrl()"></td><td><input placeholder="max" id="contrast-max-b" onchange="changeTmsUrl()"></td></tr>
-    </table>
-  </div>
-</div>
-`,
-    position: 'top'
+      <h3>Details</h3>
+      
+      <div>
+        <strong>Identifier:</strong> <em id="details-identifier"></em>
+      </div>
+      
+      <div>
+        <strong>Captured:</strong> <span id="details-datetime"></span><br>
+        <strong>Cloud Coverage:</strong> <span id="details-cloudcoverage"></span>&nbsp;%<br>
+        <!--<strong>UTM Zone:</strong><br>-->
+      </div>
+      
+      <div>
+        <strong>Opacity:</strong>
+        <input type="range" min="0" max="100" step="1" value="100" id="details-opacity" onchange="changeOpacity(event)" />
+        <span>100&nbsp;%</span>
+      </div>
+      
+      <div>
+        <strong>Color mode:</strong>
+        <input type="radio" name="colormode" id="grayscale" value="grayscale" checked/><label for="grayscale">Grayscale</label>
+        <input type="radio" name="colormode" id="rgb" value="rgb"/><label for="rgb">RGB</label>
+        <div>
+          <strong>Band to display:</strong> <select id="details-availablebands" onchange="changeTmsUrl()"></select>
+          <input placeholder="min" id="contrast-min" onchange="changeTmsUrl()">
+          <input placeholder="max" id="contrast-max" onchange="changeTmsUrl()">
+        </div>
+        <div>
+          <table>
+            <tr><td><strong>Red:  </strong></td><td><select id="details-availablebands-red"   onchange="changeTmsUrl()"></select></td><td><input placeholder="min" id="contrast-min-r" onchange="changeTmsUrl()"></td><td><input placeholder="max" id="contrast-max-r" onchange="changeTmsUrl()"></td></tr>
+            <tr><td><strong>Green:</strong></td><td><select id="details-availablebands-green" onchange="changeTmsUrl()"></select></td><td><input placeholder="min" id="contrast-min-g" onchange="changeTmsUrl()"></td><td><input placeholder="max" id="contrast-max-g" onchange="changeTmsUrl()"></td></tr>
+            <tr><td><strong>Blue: </strong></td><td><select id="details-availablebands-blue"  onchange="changeTmsUrl()"></select></td><td><input placeholder="min" id="contrast-min-b" onchange="changeTmsUrl()"></td><td><input placeholder="max" id="contrast-max-b" onchange="changeTmsUrl()"></td></tr>
+          </table>
+        </div>
+      </div>`
   });
   
   // IMPRINT panel
   sidebar.addPanel({
     id: 'imprint',
     tab: '<i class="fa fa-info-circle fa-lg"></i>',
+    position: 'bottom',
     pane: `
       <h3>Legal stuff</h3>
       <p>Amun Software Inc.</p>
-      <p>Devs: Gözde, Philipp², Niklas, Christoph</p>
-    `,
-    position: 'bottom'
+      <p>Devs: Gözde, Philipp², Niklas, Christoph</p>`
   });
   
   // SHOWING/HIDING stuff when appropriate
   sidebar.on('content', function(e) {
     if(e.id=='search') {
-      $.get('http://gis-bigdata:11016/datasets', function(result) {
-        document.getElementById('searchresults').innerHTML = result
-        .sort((e1,e2) => e1.MTD.metadata[''].PRODUCT_START_TIME < e2.MTD.metadata[''].PRODUCT_START_TIME)
-        .map((e) =>
-          `<li
-            onclick="showFootprintOnMap(event)"
-            ondblclick="showDetails(event)"
-            data-footprint="${e.MTD.metadata[''].FOOTPRINT}"
-            data-polygoncoords="${JSON.stringify(getPolygonCoords(e.MTD.metadata[''].FOOTPRINT))}"
-            data-tmsurls="${(e.tmsUrls.R10m != undefined ? Object.values(e.tmsUrls.R10m).join(',').concat(Object.values(e.tmsUrls.R20m).join(',').concat(Object.values(e.tmsUrls.R60m).join(','))) : Object.values(e.tmsUrls).join(','))}"
-            data-scenename="${e.sceneName}"
-            data-datetime="${e.MTD.metadata[''].DATATAKE_1_DATATAKE_SENSING_START}"
-            data-cloudcoverage="${e.MTD.metadata[''].CLOUD_COVERAGE_ASSESSMENT}">
-            <strong>${e.sceneName.replace(/_/g, '_&#8203;')}</strong>
-            <br>
-            ${new Date(e.MTD.metadata[''].PRODUCT_START_TIME).toLocaleString()}
-          </li>`
-        ).join('\r\n');
-        totalcount = result.length;
-        document.getElementById('resultcount').innerHTML = totalcount;
-        filterResults();
-      });
+      getSearchResults();
       map.addControl(drawControl);
-      //bboxes.addTo(map);
     } else {
       map.removeControl(drawControl);
-      bboxes.removeFrom(map)
     }
   });
   
